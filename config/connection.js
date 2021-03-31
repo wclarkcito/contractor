6994ed7e3e01955382cdfefd8f1c6e6b8694205d
Konstantin <konstantin1020@gmail.com>
2021-03-30 19:44:34 -0700

    updates

--- config/connection.js
+++ config/connection.js
@@ -20,19 +20,3 @@ if (process.env.JAWSDB_URL) {
 }
 
 module.exports = sequelize;
-
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
-
-module.exports = sequelize;
