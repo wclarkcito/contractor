8a8ba4f8cd10e874cfbf08b00814cd8f9c666ace
Raffee <Raffee1989@gmail.com>
2021-04-02 13:06:02 -0700

    handlbars are updated

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,13 +2,13 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
+  const quote = document.querySelector('#project-funding').value.trim();
   const description = document.querySelector('#project-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  if (name && quote && description) {
     const response = await fetch(`/api/projects`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ title, quote, body }),
       headers: {
         'Content-Type': 'application/json',
       },
