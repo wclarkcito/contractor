4dadab7678ff34f17cd1764fe692ac8ed23bb382
warren clark <wclarkcito@gmail.com>
2021-04-02 18:13:46 -0700

    projectsData now seeding

--- models/Projects.js
+++ models/Projects.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class Projects extends Model { }
+class Projects extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 Projects.init(
     {
@@ -41,12 +46,27 @@ Projects.init(
 
 
             references: {
-                model: 'user',
+                model: 'projects',
                 key: 'id'
             }
         },
     },
+
+
     {
+
+        // hooks: {
+        //     beforeCreate: async (newProjectsData) => {
+        //         newProjectsData.password = await bcrypt.hash(newProjectsData.password, 10);
+        //         return newProjectsData;
+        //     },
+        //     beforeUpdate: async (updatedData) => {
+        //         updatedData.password = await bcrypt.hash(updatedData.password, 10);
+        //         return updatedData;
+        //     },
+        // },
+
+
         sequelize,
         timestamps: false,
         freezeTableName: true,
