895ed9b8b7d6ab6a4c6ac196a676f19b84ffa437
Raffee <Raffee1989@gmail.com>
2021-04-05 13:10:42 -0700

    Pulling Data

--- public/js/login.js
+++ public/js/login.js
@@ -4,7 +4,7 @@ const loginFormHandler = async (event) => {
   // Collect values from the login form
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
-
+  
   if (email && password) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
