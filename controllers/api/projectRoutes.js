6cd2127e65fd51be869d0e4df6696e9d495ab214
Raffee <Raffee1989@gmail.com>
2021-04-06 19:52:01 -0700

    pulling data

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -5,26 +5,26 @@ const { signup, getBill } = require('../../controller/appController');
 
 // Returns a list of all projects
 // Route located at /api/projects
-router.get("/", async (req, res) => {
-    try {
-      const getAllProjects = await Projects.findAll();
-      res.status(200).json(getAllProjects);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+// router.get("/", async (req, res) => {
+//     try {
+//       const getAllProjects = await Projects.findAll();
+//       res.status(200).json(getAllProjects);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
 // Returns a specific project by id
-// Route located at /api/projects/:id
-router.get("/:id", async (req, res) => {
-    try {
-      const getOneProject = await Projects.findByPk(req.params.id, {
-      });
-      res.status(200).json(getOneProject);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+// // Route located at /api/projects/:id
+// router.get("/:id", async (req, res) => {
+//     try {
+//       const getOneProject = await Projects.findByPk(req.params.id, {
+//       });
+//       res.status(200).json(getOneProject);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
 // Creates a project
 // Route located at /api/projects
@@ -42,27 +42,27 @@ router.post('/', withAuth, async (req, res) => {
 
 // Updates contractor_id on project when contractor bids
 // Route located at /api/projects/:id
-router.put('/:id', withAuth, async (req, res) => {
-  try {
-    const contractor = await User.findByPk(req.session.user_id)
-    const project = await Projects.findByPk(req.params.id)
-    const homeowner = await User.findByPk(project.homeowner_id)
-    console.log(homeowner.email)
-      const updatedProject = await Projects.update({
-          // new info
-          contractor_id: req.session.user_id
-      }, {
-          where: {
-              id: req.params.id
-          }
-      })
+// router.put('/:id', withAuth, async (req, res) => {
+//   try {
+//     const contractor = await User.findByPk(req.session.user_id)
+//     const project = await Projects.findByPk(req.params.id)
+//     const homeowner = await User.findByPk(project.homeowner_id)
+//     console.log(homeowner.email)
+//       const updatedProject = await Projects.update({
+//           // new info
+//           contractor_id: req.session.user_id
+//       }, {
+//           where: {
+//               id: req.params.id
+//           }
+//       })
 
 
-      res.status(200).json(homeowner);
-  } catch (err) {
-      res.status(500).json(err);
-    }
-  })
+//       res.status(200).json(homeowner);
+//   } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   })
 
 // Deletes a project by id
 // Route located at /api/projects/:id
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,6 +29,27 @@ router.get('/', async (req, res) => {
     }
   });
 
+  // router.get('/project/:id', async (req, res) => {
+  //   try {
+  //     const projectData = await Project.findByPk(req.params.id, {
+  //       include: [
+  //         {
+  //           model: User,
+  //           attributes: ['name'],
+  //         },
+  //       ],
+  //     });
+  
+  //     const project = projectData.get({ plain: true });
+  
+  //     res.render('project', {
+  //       ...project,
+  //       logged_in: req.session.logged_in
+  //     });
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
+  // });
   router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in && req.session.user_type === "homeowner") {
