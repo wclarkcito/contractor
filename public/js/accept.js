c31099ff54a9aba36e2b51de6706a273e05c75e6
Raffee <Raffee1989@gmail.com>
2021-04-08 11:06:44 -0700

    Delete button on ContProfile Handlbar is working now

--- public/js/accept.js
+++ public/js/accept.js
@@ -47,25 +47,8 @@ const acceptProject = async () => {
     alert("Failed to create project");
   }
 }
-
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute("data-id")) {
-    const id = event.target.getAttribute("data-id");
-
-    const response = await fetch(`/api/projects/${id}`, {
-      method: "DELETE",
-    });
-
-    if (response.ok) {
-      document.location.replace("/profile");
-    } else {
-      alert("Failed to delete project");
-    }
-  }
-};
-
-// const nodeMailer = async () => {
-
-// }
-
 document.getElementById("accepted").addEventListener("click", acceptProject);
+
+
+
+
